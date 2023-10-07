1e415884caba67197286b95421b0c7d69d492f70
tayoolaigbe <tayoolaigbe@outlook.com>
2023-10-07 23:20:34 +0100

    Completed the Layout and Mobile sidebar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,7 +1,11 @@
+import { NavbarRoutes } from '@/components/navbar-routes';
+import MobileSidebar from './mobile-sidebar';
+
 const Navbar = () => {
 	return (
 		<div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
-			navbar
+			<MobileSidebar />
+			<NavbarRoutes />
 		</div>
 	);
 };
