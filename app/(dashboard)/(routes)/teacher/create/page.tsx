1067ec09a8dea1ba2b5c2bf8a18d03a049335856
tayoolaigbe <tayoolaigbe@outlook.com>
2024-01-04 07:41:09 +0000

    Added the uploadthing library to manage file uploads

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as z from 'zod';
-import axios, { Axios } from 'axios';
+import axios from 'axios';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
